3fa5d89f97b3bbc77533017c894ff1cbeb5f0f89
Andy Lang <djdreviz@gmail.com>
2024-12-17 03:55:44 +0200

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/andylang/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andylang.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
